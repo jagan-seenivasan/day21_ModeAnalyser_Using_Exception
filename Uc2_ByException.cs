d516f8ebcb5d9f304b89de4ff3f01b75c5117484
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-28 21:50:43 +0530

    analyse mood using Exception

---
+++ Uc2_ByException.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public  class Uc2_ByException
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+
+                if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch(NullReferenceException)
+            {
+                return "happy";
+
+            }
+        }
+    }
+}
