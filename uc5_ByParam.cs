88d67c168397cb0d63ecdd94fe178ee8406a2571
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-28 21:57:35 +0530

    analyse mood by parameterised constructor

---
+++ uc5_ByParam.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public  class uc5_ByParam
+    {
+        string message;
+
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
+
+                else if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch(NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.NULL_EXCEPTION, "Mood should not be null");
+
+            }
+        }
+    }
+}
